784905fcd373b6035592fc33b90753c7f6967b86
Beza Mezgebu <bezamezgebu.2010@gmail.com>
2020-02-02 13:28:06 -0800

    Add /books/search route

--- TechLibrary/Controllers/BooksController.cs
+++ TechLibrary/Controllers/BooksController.cs
@@ -6,6 +6,8 @@ using AutoMapper;
 using TechLibrary.Domain;
 using TechLibrary.Models;
 using TechLibrary.Services;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace TechLibrary.Controllers
 {
@@ -16,6 +18,7 @@ namespace TechLibrary.Controllers
         private readonly ILogger<BooksController> _logger;
         private readonly IBookService _bookService;
         private readonly IMapper _mapper;
+
 
         public BooksController(ILogger<BooksController> logger, IBookService bookService, IMapper mapper)
         {
@@ -54,6 +57,29 @@ namespace TechLibrary.Controllers
 
             return Ok(bookResponseList);
         }
+
+        [HttpGet("search")]
+        public async Task<IActionResult> GetBooksByTitle(string title)
+        {
+            _logger.LogInformation("Get book by title");
+
+            var allBooks = await _bookService.GetBooksAsync();
+
+            var filteredList = new List<Book>();
+            foreach (Book book in allBooks)
+            {
+                //if the book title contains the searched title add to the filterd list
+                if (book.Title.Contains(title))
+                {
+                    filteredList.Add(book);
+                }
+            }
+
+            var bookResponseList = _mapper.Map<List<BookResponse>>(filteredList);
+
+            return Ok(bookResponseList);
+        }
+        
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(int id)
