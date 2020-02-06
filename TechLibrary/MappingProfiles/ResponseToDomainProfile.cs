7ccff7ed394783aea60b1cb49804b0d2d7ac5ff7
Beza Mezgebu <bezamezgebu.2010@gmail.com>
2020-02-05 18:38:35 -0800

    Add mapping profile to map from response to domain object

---
+++ TechLibrary/MappingProfiles/ResponseToDomainProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TechLibrary.Domain;
+using TechLibrary.Models;
+
+namespace TechLibrary.MappingProfiles
+{
+    public class ResponseToDomainProfile : Profile
+    {
+        public ResponseToDomainProfile()
+        {
+            CreateMap<BookResponse, Book>().ForMember(x => x.ShortDescr, opt => opt.MapFrom(src => src.Descr));
+        }
+    }
+}
